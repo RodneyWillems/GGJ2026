01382f7eddea511bd31e2ee5dc1050091a644f9f
Loïc <loic.rutte@gmail.com>
2026-01-30 11:39:51 +0100

    door breaking

--- Assets/Devs/Scripts/DoorZone.cs
+++ Assets/Devs/Scripts/DoorZone.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class DoorZone : MonoBehaviour
 {
     [SerializeField] private Animation closeDoor;
+
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
--- Assets/Devs/Scripts/Interact/Wall.cs
+++ Assets/Devs/Scripts/Interact/Wall.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class Wall : Interactable
 {
-    [SerializeField] private GameObject wallWhole;
+   // [SerializeField] private GameObject wallWhole;
     [SerializeField] private GameObject wallBroken;
+
+    private void Start()
+    {
+        wallBroken.SetActive(false);
+
+    }
 
     public override void Interact(PlayerMovement player)
     {
-        if (player.CheckInventory("axe"))
-        {
-            wallWhole.SetActive(false);
+        print("Player interacted");
+       // if (player.CheckInventory("axe"))
+       // {
+            gameObject.SetActive(false);
             wallBroken.SetActive(true);
-        }
+      // }
     }
 }
